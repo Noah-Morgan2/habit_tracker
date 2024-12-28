e0fa49ff45b2fa4ec857208989bac8359c2a0956
Noah <161484244+Noah-Morgan2@users.noreply.github.com>
2024-12-28 21:32:39 +0800

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const habitRouter = require('./routers/habitRouter')
+const dateRouter = require('./routers/dateRouter')
+const authRouter = require('./routers/authRouter') 
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(bodyParser.json())
+
+app.use(middleware.tokenExtractor)
+
+app.use('/api/habits', habitRouter)
+app.use('/api/dates', dateRouter)
+app.use('/api/auth', authRouter)
+
+module.exports = app
+
